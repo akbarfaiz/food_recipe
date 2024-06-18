da85e7d81c1a87c14a9612e2f4f53ae71fa6a25f
Akbar Faiz <87055460+akbarfaiz@users.noreply.github.com>
2024-06-18 14:39:59 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+var cors = require('cors')
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const mainRouter = require('./src/routes')
+const ErrorHandler = require('./src/middleware/errorHandler')
+
+const app = express()
+const port = 4000
+
+app.use(cors({
+  origin: "*",
+  method:"*"
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use("/",mainRouter)
+app.use('/img',express.static('./tmp'))
+
+app.use('*',function(req, res) {
+        res.status(404).json({
+          status: 404,
+           message: 'Invalid Request'
+        });
+  });
+
+//Error Handling
+app.use(ErrorHandler)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
